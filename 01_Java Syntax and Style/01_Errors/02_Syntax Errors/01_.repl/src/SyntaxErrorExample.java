4c165408068aeae10c0102a48574ef053bc1baa7
Stephen Lee <stephenlee@Stephens-MacBook-Air.local>
2018-08-01 18:59:18 +0800

    Add sample code

---
+++ 01_Java Syntax and Style/01_Errors/02_Syntax Errors/01_.repl/src/SyntaxErrorExample.java
@@ -0,0 +1,20 @@
+package exlhub;
+public class SyntaxErrorExample {
+    /*
+    // public cannot be capitalized
+    // ints should be int
+    Public static ints exampleVariableOne = 5;
+
+    // static cannot be capitalized
+    public Static void main (String[] args){
+        // printline should be println
+        System.out.printline("Value of exampleVariableOne is " + exampleVariableOne);
+    }
+    */
+
+  public static int exampleVariableOne = 5;
+
+  public static void main(String[] args) {
+    System.out.println("Value of exampleVariableOne is " + exampleVariableOne);
+  }
+}
