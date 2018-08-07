777c8781098ae766b923330b198478cdc9582aab
Stephen Lee <stephenlee@Stephens-MacBook-Air.local>
2018-08-07 11:15:05 +0800

    fix spelling for variable name

--- 07_Object Oriented Programming/00_Objects/01_Object Equality/01_.repl/src/ObjectEqualityTest.java
+++ 07_Object Oriented Programming/00_Objects/01_Object Equality/01_.repl/src/ObjectEqualityTest.java
@@ -2,7 +2,7 @@ package exlcode;
 
 public class ObjectEqualityTest {
 
-  private String exampleVaraibleThree;
+  private String exampleVariableThree;
 
   public ObjectEqualityTest(String exampleVaraibleThree) {
     this.exampleVaraibleThree = exampleVaraibleThree;
